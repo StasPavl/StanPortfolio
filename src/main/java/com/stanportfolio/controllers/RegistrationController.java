d8f379a4c053dc168b081d45ef4e3563f79c22e8
lichfb <lichfb@gmail.com>
2024-03-15 15:01:28 -0400

    Fourth Day

--- src/main/java/com/stanportfolio/controllers/RegistrationController.java
+++ src/main/java/com/stanportfolio/controllers/RegistrationController.java
@@ -7,27 +7,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/registration")
 public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping("/newuser")
-    public String registerNewUser(@ModelAttribute("newUser") UserCredentialsDto userCredentials){
+    public String  registerNewUser(@ModelAttribute("newUser") UserCredentialsDto userCredentials){
+
         if (Objects.isNull(userCredentials))
             return "redirect:/register?noCreds";
 
         if (registrationService.userExists(userCredentials))
             return "redirect:/register?userExists";
 
-        if (Objects.equals(userCredentials.getPassword(), userCredentials.getRePassword()))
+        if (!Objects.equals(userCredentials.getPassword(), userCredentials.getRePassword()))
             return "redirect:/register?passMissMatch";
 
         var dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
@@ -41,6 +44,7 @@ public class RegistrationController {
             model.setLastUpdated(model.getCreatedOn());
         registrationService.save(model);
         return "redirect:/index";
+
 
     }
 }
--- src/main/java/com/stanportfolio/data/dto/UserCredentialsDto.java
+++ src/main/java/com/stanportfolio/data/dto/UserCredentialsDto.java
@@ -1,8 +1,10 @@
 package com.stanportfolio.data.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserCredentialsDto {
 
     private String username;
